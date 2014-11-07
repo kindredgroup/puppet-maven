07691e7d0b360c9cf9a77f82960233d44bedf0d3
Johan Lyheden <Johan.Lyheden@UNIBET.com>
2014-11-07 14:32:17 +0100

    updated acceptance tests

--- test/integration/default/puppet/manifests/site.pp
+++ test/integration/default/puppet/manifests/site.pp
@@ -2,96 +2,136 @@ class { '::maven':
   package_ensure  => '3.1.1'
 }
 
-maven::settings { 'vagrant_settings_xml':
-  ensure  => present,
-  user    => 'vagrant',
-  path    => '/home/vagrant/.m2/settings.xml'
+user { 'test1':
+  ensure      => present,
+  home        => '/home/test1',
+  managehome  => true
 }
 
-maven::settings::mirror { 'Local Proxy of central repo':
-  id        => 'central-proxy',
-  url       => 'http://repo.example.com/repo/content/groups/public',
-  mirror_of => '*,!releases-internal-releases,!releases-internal-snapshots,!releases-plugins-internal-releases,!releases-plugins-internal-snapshots',
-  path      => '/home/vagrant/.m2/settings.xml',
-  tag_name  => 'vagrant_settings_xml'
-}
+  ::maven::settings { 'test1':
+    ensure  => present,
+    path    => '/home',
+    mirrors => [
+      {
+        id        => 'central-proxy',
+        name      => 'Local Proxy of central repo',
+        url       => 'http://nexus.company.com/nexus/content/groups/public/',
+        mirror_of => '*,!releases-internal-releases,!releases-internal-snapshots,!releases-plugins-internal-releases,!releases-plugins-internal-snapshots'
+      }
+    ],
 
-maven::settings::profile { 'vagrant_settings_profile':
-  id                => 'default',
-  path              => '/home/vagrant/.m2/settings.xml',
-  tag_name          => 'vagrant_settings_xml',
-  active_by_default => true,
-  properties        => [
-    {
-      key   => 'releases.repository.url',
-      value => 'http://repo.example.com/repo/content/repositories/releases-hosted'
-    },
-    {
-      key   => 'snapshots.repository.url',
-      value => 'http://repo.example.com/repo/content/repositories/snapshots'
-    },
-    {
-      key   => 'maven.test.error.ignore',
-      value => 'true'
-    },
-    {
-      key   => 'maven.test.failure.ignore',
-      value => 'true'
-    }
-  ],
-  repositories      => [
-    {
-      id        => 'releases-internal-snapshots',
-      url       => 'http://repo.example.com/repo/content/repositories/snapshots',
-      layout    => 'default',
-      releases  => false,
-      snapshots => true
-    },
-    {
-      id        => 'releases-internal-releases',
-      url       => 'http://repo.example.com/repo/content/groups/releases',
-      layout    => 'default',
-      releases  => true,
-      snapshots => false
-    },
-    {
-      id        => 'releases-public',
-      url       => 'http://repo.example.com/repo/content/groups/public',
-      layout    => 'default',
-      releases  => true,
-      snapshots => false
-    }
-  ],
-  plugin_repositories => [
-    {
-      id        => 'releases-plugins-internal-snapshots',
-      url       => 'http://repo.example.com/repo/content/repositories/snapshots',
-      layout    => 'default',
-      releases  => false,
-      snapshots => true
-    },
-    {
-      id        => 'releases-plugins-internal-releases',
-      url       => 'http://repo.example.com/repo/content/groups/releases',
-      layout    => 'default',
-      releases  => true,
-      snapshots => false
-    }
-  ]
-}
+    #
+    # Profiles
+    #
+    profiles  => [
+      {
+        id                => 'default',
+        active_by_default => true,
 
-maven::settings::server { 'm2-internal-official':
-  id        => 'm2-internal-official',
-  username  => 'username',
-  password  => 'password',
-  path      => '/home/vagrant/.m2/settings.xml',
-  tag_name  => 'vagrant_settings_xml'
-}
+    #
+    # Profile properties
+    #
+        properties        => [
+          {
+            key   => 'releases.repository.url',
+            value => 'http://nexus.company.com/nexus/content/repositories/releases-hosted'
+          },
+          {
+            key   => 'snapshots.repository.url',
+            value => 'http://nexus.company.com/nexus/content/repositories/snapshots'
+          },
+          {
+            key   => 'sonar.jdbc.url',
+            value => 'jdbc:oracle:thin:@db.company.com/site_standard.db.company.com'
+          },
+          {
+            key   => 'sonar.jdbc.driver',
+            value => 'oracle.jdbc.driver.OracleDriver'
+          },
+          {
+            key   => 'sonar.jdbc.username',
+            value => 'USERNAME'
+          },
+          {
+            key   => 'sonar.jdbc.password',
+            value => 'PASSWORD'
+          },
+          {
+            key   => 'maven.test.error.ignore',
+            value => 'true'
+          },
+          {
+            key   => 'maven.test.failure.ignore',
+            value => 'true'
+          },
+          {
+            key   => 'sonar.host.url',
+            value => 'http://sonar.company.com'
+          }
+        ],
 
-maven::settings::server { 'm2-internal-devel':
-  id        => 'm2-internal-devel',
-  username  => 'username',
-  password  => 'password',
-  path      => '/home/vagrant/.m2/settings.xml',
-  tag_name  => 'vagrant_settings_xml'
-}
+    #
+    # Profile repositories
+    #
+        repositories    => [
+          {
+            id        => 'releases-internal-snapshots',
+            url       => 'http://nexus.company.com/nexus/content/repositories/snapshots',
+            layout    => 'default',
+            releases  => false,
+            snapshots => true
+          },
+          {
+            id        => 'releases-internal-releases',
+            url       => 'http://nexus.company.com/nexus/content/groups/releases',
+            layout    => 'default',
+            releases  => true,
+            snapshots => false
+          },
+          {
+            id        => 'releases-public',
+            url       => 'http://nexus.company.com/nexus/content/groups/public',
+            layout    => 'default',
+            releases  => true,
+            snapshots => false
+          }
+        ],
+
+    #
+    # Profile plugin repositories
+    #
+        plugin_repositories => [
+          {
+            id        => 'releases-plugins-internal-snapshots',
+            url       => 'http://nexus.company.com/nexus/content/repositories/snapshots',
+            layout    => 'default',
+            releases  => false,
+            snapshots => true
+          },
+          {
+            id        => 'releases-plugins-internal-releases',
+            url       => 'http://nexus.company.com/nexus/content/groups/releases',
+            layout    => 'default',
+            releases  => true,
+            snapshots => false
+          }
+        ]
+      }
+    ],
+
+    #
+    # Servers
+    #
+    servers   => [
+      {
+        id        => 'm2-internal-official',
+        username  => 'USERNAME',
+        password  => 'PASSWORD'
+      },
+      {
+        id        => 'm2-internal-devel',
+        username  => 'USERNAME',
+        password  => 'PASSWORD'
+      }
+    ]
+  }
